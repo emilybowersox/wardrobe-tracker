62f3a987e46f54b257202c1bb05e4b1994e158e8
Emily Bowersox <Emily.Bowersox@improving.com>
2021-09-23 14:32:34 -0500

    basic file structure

---
+++ WardrobeTracker/Models/Outfit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WardrobeTracker.Models
+{
+    public class Outfit
+    {
+    }
+}
